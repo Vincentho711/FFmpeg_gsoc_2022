8a01bcd87b97fa152bed5f6455c0324ec4f52e41
Timo Rothenpieler <timo@rothenpieler.org>
2021-09-28 20:45:26 +0200

    avcodec/nvenc: copy incoming hwframes when applicable

--- libavcodec/nvenc.c
+++ libavcodec/nvenc.c
@@ -1910,19 +1910,28 @@ static int nvenc_upload_frame(AVCodecContext *avctx, const AVFrame *frame,
     NvencDynLoadFunctions *dl_fn = &ctx->nvenc_dload_funcs;
     NV_ENCODE_API_FUNCTION_LIST *p_nvenc = &dl_fn->nvenc_funcs;
 
-    int res;
+    int res, reg_idx;
     NVENCSTATUS nv_status;
 
     if (avctx->pix_fmt == AV_PIX_FMT_CUDA || avctx->pix_fmt == AV_PIX_FMT_D3D11) {
-        int reg_idx = nvenc_register_frame(avctx, frame);
-        if (reg_idx < 0) {
-            av_log(avctx, AV_LOG_ERROR, "Could not register an input HW frame\n");
-            return reg_idx;
-        }
-
         res = av_frame_ref(nvenc_frame->in_ref, frame);
         if (res < 0)
             return res;
+
+        if (avctx->pix_fmt == AV_PIX_FMT_CUDA && !ctx->lowmem) {
+            res = av_frame_make_writable(nvenc_frame->in_ref);
+            if (res < 0) {
+                av_frame_unref(nvenc_frame->in_ref);
+                return res;
+            }
+        }
+
+        reg_idx = nvenc_register_frame(avctx, nvenc_frame->in_ref);
+        if (reg_idx < 0) {
+            av_log(avctx, AV_LOG_ERROR, "Could not register an input HW frame\n");
+            av_frame_unref(nvenc_frame->in_ref);
+            return reg_idx;
+        }
 
         if (!ctx->registered_frames[reg_idx].mapped) {
             ctx->registered_frames[reg_idx].in_map.version = NV_ENC_MAP_INPUT_RESOURCE_VER;
